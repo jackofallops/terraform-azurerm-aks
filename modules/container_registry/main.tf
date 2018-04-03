7888e87c81e42923d63dc066edea6bffd7a2faa5
sjones-sot <steve@sourceoftruth.co.uk>
2018-04-03 12:25:39 +0100

    Updated container registstry module to account for exclusive Resource Group requirement for AKS.  commented lifecycle protection while testing/developing

--- modules/container_registry/main.tf
+++ modules/container_registry/main.tf
@@ -1,13 +1,18 @@
+resource "azurerm_resource_group" "container_registry_rg" {
+  location = "${var.location}"
+  name     = "${var.registry_name}-rg"
+}
+
 resource "azurerm_container_registry" "container_registry" {
   count               = "${var.create_registry == "true" ? 1 : 0 }"
   name                = "${var.registry_name}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group_name}"
+  location            = "${azurerm_resource_group.container_registry_rg.location}"
+  resource_group_name = "${azurerm_resource_group.container_registry_rg.name}"
   sku                 = "${var.sku}"
 
   tags = "${var.tags}"
 
   lifecycle {
-    prevent_destroy = true
+//    prevent_destroy = true
   }
 }
