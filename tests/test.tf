9723942cbf7ad5668b71a3c6e42bce0fcf758cb9
sjones-sot <steve@sourceoftruth.co.uk>
2018-04-03 14:04:00 +0100

    Updated tests with minimum default values to run a plan without input

--- tests/test.tf
+++ tests/test.tf
@@ -1,6 +1,6 @@
 module "test_aks" {
-  source           = "../"
-  sp_client_id     = "${var.sp_client_id}"
-  sp_client_secret = "${var.sp_client_secret}"
-
+  source                    = "../"
+  sp_client_id              = "${var.sp_client_id}"
+  sp_client_secret          = "${var.sp_client_secret}"
+  create_container_registry = "${var.create_container_registry}"
 }
--- tests/variables.tf
+++ tests/variables.tf
@@ -1,6 +1,12 @@
-variable "sp_client_id" {}
+variable "sp_client_id" {
+  default = "356523C7-A257-4740-B4F1-C96FCE0E0F5D"
+}
 
-variable "sp_client_secret" {}
+variable "sp_client_secret" {
+  default = "356523C7-A257-4740-B4F1-C96FCE0E0F5D"
+}
 
-variable "create_container_registry" {}
-
+variable "create_container_registry" {
+  default = "true"
+//  default = "false"
+}
